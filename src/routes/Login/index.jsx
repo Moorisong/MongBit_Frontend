3984dafb74b03e8b26117c07a98c6745ff4aa234
Moorisong <thiagoo@naver.com>
2023-06-06 17:45:10 +0900

    fix: Kakao login api test

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -1,28 +1,28 @@
 import axios from "axios"
 import { useEffect, useState } from "react";
+// import { useNavigate } from 'react-router'
 
 export default function Login() {
-  const [data, setData] = useState(null);
+  const [redirectUri, setRedirectUri] = useState(null);
+  // const navigate = useNavigate()
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const url = 'https://kauth.kakao.com/oauth/authorize'
-        const param = {
-          client_id: '3245a5f9cb8303814aadbe1eb65b2e73',
-          redirect_uri: 'https://mongbit-willneiman.koyeb.app',
-          response_type: 'code'
-        }
-        const response = await axios.get(url);
-        setData(response.data);
+        // const oldUrl = 'https://kauth.kakao.com/oauth/authorize?client_id=3245a5f9cb8303814aadbe1eb65b2e73&redirect_uri=https://mongbit-willneiman.koyeb.app/login/oauth2/kakao/code&response_type=code'
+        const response = await axios.get('https://mongbit-willneiman.koyeb.app/login/oauth2/kakao/url');
+        setRedirectUri(response.data);
+
+        console.log('redirectUri---> ', redirectUri)
+
+        if(redirectUri) window.location.href = redirectUri;
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchData();
-    console.log('data---> ', data)
-  }, []);
+  }, [redirectUri]);
   return (
     <>
       <div>
