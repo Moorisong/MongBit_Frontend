d6daa4295368f05c5263ca3cb48b547803d17ea4
Moorisong <thiagoo@naver.com>
2023-06-08 12:13:10 +0900

    fix: Set local storage with token

--- src/routes/KakaoAuthHandle/index.jsx
+++ src/routes/KakaoAuthHandle/index.jsx
@@ -7,18 +7,17 @@ export default function KakaoAuthHandle() {
   const [searchParams] = useSearchParams();
   const code = searchParams.get('code');
 
-
-  console.log("code--> ", code)
-
   useEffect(() => {
 
     const handleKakaoAuth = async () => {
       if (code) {
         try {
           const response = await axios.get(`https://mongbit-willneiman.koyeb.app/login/oauth2/kakao/code?code=${code}`);
-          console.log('res--> ', response)
+          console.log('response.data --> ', response.data)
+          console.log('response.headers.authorizationa --> ', response.headers.authorization)
+
           localStorage.setItem('token', response.headers.authorization);
-          // navigate('/');
+          // navigate('/')
         } catch (error) {
           console.error(error);
         }
