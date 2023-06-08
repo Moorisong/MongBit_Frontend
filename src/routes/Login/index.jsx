264c2c9cc7614654a0a687faf0eb6e411af506e4
Moorisong <thiagoo@naver.com>
2023-06-08 14:38:15 +0900

    fix: Divide components by login step(2)

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -7,15 +7,18 @@ import jwtDecode from "jwt-decode";
 export default function Login() {
   const [redirectUri, setRedirectUri] = useState(null);
 
-  const url = 'https://kauth.kakao.com/oauth/authorize?client_id=3245a5f9cb8303814aadbe1eb65b2e73&redirect_uri=https://mongbit-frontend-moorisong.koyeb.app/login/oauth2/kakao/code&response_type=code'
+  console.log(process.env.REACT_APP_FE_URL);
+  console.log(process.env.NODE_ENV);
+
+  const url = `https://kauth.kakao.com/oauth/authorize?client_id=3245a5f9cb8303814aadbe1eb65b2e73&redirect_uri=${process.env.REACT_APP_FE_URL}/login/oauth2/kakao/code&response_type=code`
 
   const kakaoLogin = () => {
     window.location.href = url;
-};
+  };
 
   return (
     <>
-     <button onClick={kakaoLogin}>login</button>
+      <button onClick={kakaoLogin}>login</button>
 
     </>
   )
