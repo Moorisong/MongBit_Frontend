8a776e4c2cacad5b4af5bae3b98a6b9ce68c7570
Moorisong <thiagoo@naver.com>
2023-06-09 17:42:10 +0900

    fix: Bug of Kakao login validattion
    - 카카오 인가코드 수신을 위한 리다이렉트 주소를 로컬 주소가 아닌 배포된 주소로 변경해줌

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -10,7 +10,9 @@ export default function Login() {
   // console.log(process.env.REACT_APP_FE_URL);
   // console.log(process.env.NODE_ENV);
 
-  const url = `https://kauth.kakao.com/oauth/authorize?client_id=3245a5f9cb8303814aadbe1eb65b2e73&redirect_uri=${process.env.REACT_APP_FE_URL}/login/oauth2/kakao/code&response_type=code`
+  const url = process.env.REACT_APP_FE_URL ?
+    `https://kauth.kakao.com/oauth/authorize?client_id=3245a5f9cb8303814aadbe1eb65b2e73&redirect_uri=${process.env.REACT_APP_FE_URL}/login/oauth2/kakao/code&response_type=code`
+    : `https://kauth.kakao.com/oauth/authorize?client_id=3245a5f9cb8303814aadbe1eb65b2e73&redirect_uri=https://mongbit-frontend-moorisong.koyeb.app/login/oauth2/kakao/code&response_type=code`
 
   const kakaoLogin = () => {
     window.location.href = url;
