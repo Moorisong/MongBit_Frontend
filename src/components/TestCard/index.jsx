f60b82436bde1027099380e449ba50e86c29557d
Moorisong <thiagoo@naver.com>
2023-06-10 18:16:43 +0900

    fix: Make the constant state of css state

--- src/components/TestCard/index.jsx
+++ src/components/TestCard/index.jsx
@@ -1,11 +1,16 @@
 import styles from './index.module.css'
+import { TYPE_LATEST_MAIN } from '../../constants/constant'
 
 function TestCard(props) {
 
+  const cn_1 = props.type === TYPE_LATEST_MAIN ? `${styles.marginLeft} ${styles.inline}` : `${styles.marginLeft}`
+  const cn_2 = props.type === TYPE_LATEST_MAIN ? `${styles.latest_thumbnail}` : `${styles.normal_thumbnail}`
+  const cn_3 = props.type === TYPE_LATEST_MAIN ? `${styles.latest_titleBox}` : `${styles.normal_titleBox}`
+
   return (
-    <div className={`${styles[props.marginClass]} ${styles[props.displayClass]}`}>
-      <div className={styles[props.thumbnailClass]} ></div>
-      <div className={styles[props.titleBoxClass]}>
+    <div className={cn_1}>
+      <div className={cn_2} ></div>
+      <div className={cn_3}>
         <span className={`${styles[props.textClass]}`}>{props.thumbnailStr}</span>
       </div>
     </div>
