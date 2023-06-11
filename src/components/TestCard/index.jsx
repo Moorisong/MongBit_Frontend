63559ae05ae8add5726ba5e998b932bf257a8dc3
Moorisong <thiagoo@naver.com>
2023-06-11 17:09:34 +0900

    fix: Bug on my page
    - 구분선 사이 정렬이 불규칙적으로 표시되는 버그 개선함

--- src/components/TestCard/index.jsx
+++ src/components/TestCard/index.jsx
@@ -4,16 +4,18 @@ import { TYPE_LATEST_MAIN, TYPE_MYPAGE } from '../../constants/constant'
 function TestCard(props) {
 
   const cn_1 = props.type === TYPE_LATEST_MAIN || props.type === TYPE_MYPAGE ? `${styles.marginLeft} ${styles.inline}` : `${styles.marginLeft}`
-  const cn_2 = props.type === TYPE_LATEST_MAIN || props.type === TYPE_MYPAGE ? `${styles.latest_thumbnail}` : `${styles.normal_thumbnail}`
-  const cn_3 = props.type === TYPE_LATEST_MAIN || props.type === TYPE_MYPAGE ? `${styles.latest_titleBox}` : `${styles.normal_titleBox}`
+  const cn_2 = props.type === TYPE_LATEST_MAIN ? `${styles.latest_thumbnail}` : props.type === TYPE_MYPAGE ? `${styles.latest_thumbnail} ${styles.myPage}` : `${styles.normal_thumbnail}`
+  const cn_3 = props.type === TYPE_LATEST_MAIN ? `${styles.latest_titleBox}` : `${styles.normal_titleBox}`
 
   return (
     <div className={cn_1}>
       <div className={cn_2} ></div>
-      <div className={cn_3}>
-        <span className={`${styles[props.textClass]}`}>{props.thumbnailStr}</span>
-      </div>
-    </div>
+      {props.type === TYPE_MYPAGE ||
+        < div className={cn_3}>
+          <span className={`${styles[props.textClass]}`}>{props.thumbnailStr}</span>
+        </div>
+      }
+    </div >
   )
 }
 
--- src/components/Titles/index.jsx
+++ src/components/Titles/index.jsx
@@ -3,7 +3,7 @@ import { TITLE_WITH_CONTENT, TYPE_TEST_LIST, TYPE_MYPAGE } from '../../constants
 
 function TitleWithText(props) {
   const cn_1 = props.type_1 === TITLE_WITH_CONTENT ? `${styles.title} ${styles.withContent}` : `${styles.title}`
-  const cn_2 = props.type_2 === TYPE_TEST_LIST ? `${styles.content} ${styles.marginBottom}` : `${styles.content}`
+  const cn_2 = props.type_2 === TYPE_TEST_LIST ? `${styles.content} ${styles.marginBottom}` : props.type_2 === TYPE_MYPAGE ? `${styles.content} ${styles.myPage}` : `${styles.content}`
   return (
     <div className={styles.wrap}>
       <p className={cn_1}>{props.title}</p>
