7cff482470feb4d6607495e7d58806a1dc2e9e50
Moorisong <thiagoo@naver.com>
2023-06-12 14:14:03 +0900

    fix: Update Main page's style

--- src/routes/TestList/index.jsx
+++ src/routes/TestList/index.jsx
@@ -4,20 +4,31 @@ import NavigationBar from "../../components/NavigationBar"
 import Footer from "../../components/Footer"
 import styels from './index.module.css'
 import { TYPE_TEST_LIST, TITLE_WITH_CONTENT } from "../../constants/constant"
+import axios from "axios"
 
 export default function TestList(props) {
     const titleStr = '💛  몽빗 심테'
     const contentStr = '대충 후킹 멘트 자리 대충 후킹 멘트 자리 대충 후킹 멘트 자리'
+
+    function handleFileChange(event){
+        const file = event.target.files[0];
+        const formData = new FormData();
+        formData.append('file', file);
+
+        axios.post('http://localhost:8080/upload', formData)
+            .then((response) => {
+                console.log('re--->', response.data); 
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
     return (
         <div className={styels.containerWrap}>
             <NavigationBar />
-            <div>
-                <form name="form" action="https://mongbit-willneiman.koyeb.app/upload" method="post" enctype="multipart/form-data">
-                    <input type='file' name='file' />
-                    <input type="submit" value="Submit" />
-                </form>
 
-            </div>
+            <input type="file" name="file" onChange={handleFileChange}/>
+
             <div className={styels.titleTextWrap}>
                 <TitleWithText className={styels.titleWithText} title={titleStr} content={contentStr} type_1={TITLE_WITH_CONTENT} type_2={TYPE_TEST_LIST} />
             </div>
