3e82462ced857134b9748f46b115a642e88aea58
Moorisong <thiagoo@naver.com>
2023-06-14 15:24:35 +0900

    fix: Remove alert and update menu bar

--- src/components/NavigationBar/index.jsx
+++ src/components/NavigationBar/index.jsx
@@ -16,7 +16,6 @@ export default function NavigationBar() {
 
     function checkJwtToken() {
         if (!localStorage.getItem(TOKEN_NAME)) {
-            alert('로그인 해주세요')
             return navigate('/login')
         }
         const token = localStorage.getItem(TOKEN_NAME)
@@ -29,7 +28,6 @@ export default function NavigationBar() {
         console.log('decoded-----> ', decodedToken)
 
         if (expirationTime < currentTime) {
-            alert('로그인 해주세요')
             return navigate('/login')
 
         } else {
@@ -41,52 +39,54 @@ export default function NavigationBar() {
     function clickLogOut() {
         localStorage.setItem(TOKEN_NAME, '')
         setLogIn(false)
-        alert('로그아웃 되었습니다')
         console.log('로그아웃됨 ---- ')
         navigate('/main')
     }
 
     return (
         <div className={styles.wrap}>
-            
-                <div className={styles.navWrap}>
-                    <div className={styles.menuIcon} onClick={() => setMenuClicked(!menuClicked)}></div>
-                    <div className={styles.logoWrap}>
-                        <Link to="/main" className={styles.logoDog}></Link>
-                        <Link to="/main" className={styles.logoTitle}></Link>
-                    </div>
-                    {location.pathname === '/mypage' ? <button className={styles.myPageBtnNone}></button> : <button className={styles.myPageBtn} onClick={checkJwtToken}></button>}
+
+            <div className={styles.navWrap}>
+                <div className={styles.menuIcon} onClick={() => setMenuClicked(true)}></div>
+                <div className={styles.logoWrap} onClick={() => setMenuClicked(false)}>
+                    <Link to="/main" className={styles.logoDog}></Link>
+                    <Link to="/main" className={styles.logoTitle}></Link>
                 </div>
+                {location.pathname === '/mypage' ? <button className={styles.myPageBtnNone}></button> : <button className={styles.myPageBtn} onClick={checkJwtToken}></button>}
+            </div>
 
 
             <div className={cx(styles.menuWrap, { [styles.menuMoveToRight]: menuClicked })}>
-                    <ul>
-                        <li>
-                            <ul className={styles.ulWrap}>심리테스트
-                                <li>최신 심테</li>
-                            </ul>
-                            <Link to="/test-list">전체 보기</Link>
-                        </li>
-                        <li>
-                            <ul className={styles.ulWrap}>마이페이지
-                                <li>심테 기록 보기</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <ul className={styles.ulWrap}>개발자 정보
-                                <li>몽몽이 크루</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <ul>
-                                <li>
-                                    <button onClick={clickLogOut}>로그아웃</button>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            <div className={cx(styles.modal, { [styles.modalMoveToRight]: menuClicked })}></div>
+                <ul>
+                    <li>
+                        <ul className={styles.ulWrap}>심리테스트
+                            <li>최신 심테</li>
+                        </ul>
+                        <Link to="/test-list">전체 보기</Link>
+                    </li>
+                    <li>
+                        <ul className={styles.ulWrap}>마이페이지
+                            <li>심테 기록 보기</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <ul className={styles.ulWrap}>개발자 정보
+                            <li>몽몽이 크루</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <ul>
+                            <li className={styles.logOutWrap} >
+                                <p onClick={clickLogOut}>로그아웃</p>
+                                <button className={styles.logOutBtn} onClick={clickLogOut} ></button>
+                                <img src="/images/navigationBar/logo_dog.svg" alt="logo" />
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div className={styles.fixModal}></div>
+            <div className={cx(styles.blackModal, { [styles.modalMoveToRight]: menuClicked })} onClick={() => setMenuClicked(false)}></div>
         </div>
     );
 }
