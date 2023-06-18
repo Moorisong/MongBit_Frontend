dc2e6426af1225e8b03b635016998563f043e385
Moorisong <thiagoo@naver.com>
2023-06-18 15:09:28 +0900

    fix: the Eslint config

--- src/atom.js
+++ src/atom.js
@@ -1,7 +1,6 @@
 import { atom } from 'recoil';
 
 import { decodeToken } from './util/util';
-import { TOKEN_NAME, USER_INFO } from './constants/constant';
 
 export const tokenInfo = atom({
   key: 'tokenState',
